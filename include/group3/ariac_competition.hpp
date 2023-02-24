aa01a4b0dc7147eb93af77efa3d332d38c1e4c78
saahu27 <sahrudaypatti@gmail.com>
2023-02-23 19:24:12 -0500

    creating timer without the check for submit order

--- include/group3/ariac_competition.hpp
+++ include/group3/ariac_competition.hpp
@@ -7,20 +7,29 @@
 #include <ariac_msgs/msg/competition_state.hpp>
 #include <ariac_msgs/srv/submit_order.hpp>
 
-
+using namespace std::chrono_literals;
 class AriacCompetition : public rclcpp::Node
 {
  public:
+    unsigned int competition_state_;
     AriacCompetition(std::string node_name) : Node(node_name)
     {
         competition_state_sub_ = this->create_subscription<ariac_msgs::msg::CompetitionState>("/ariac/competition_state", 10, 
         std::bind(&AriacCompetition::competition_state_cb, this, std::placeholders::_1));
+
+        // this->declare_parameter("publishing_interval", 2);
+        // rclcpp::Parameter pub_frequency = this->get_parameter("publishing_interval");
+
+        end_competition_timer_ = this->create_wall_timer(100ms, std::bind(&AriacCompetition::end_competition_timer_callback, this));
     }
  
  private:
     rclcpp::Subscription<ariac_msgs::msg::CompetitionState>::SharedPtr competition_state_sub_;
+
+    rclcpp::TimerBase::SharedPtr end_competition_timer_;
     
     void competition_state_cb(const ariac_msgs::msg::CompetitionState::ConstSharedPtr msg);
 
+    void end_competition_timer_callback();
 
 };
--- src/ariac_competition.cpp
+++ src/ariac_competition.cpp
@@ -1,23 +1,45 @@
 #include "ariac_competition.hpp"
 
-using namespace std::chrono_literals;
+void AriacCompetition::end_competition_timer_callback() {
+  if (competition_state_ == 3) {
+    std::string srv_name = "/ariac/end_competition";
 
-// AriacCompetition::AriacCompetition() : Node ("AriacCompetition"){
-
-//     competition_state_sub_ = this->create_subscription<ariac_msgs::msg::CompetitionState>("/ariac/competition_state", 10, 
-//     std::bind(&AriacCompetition::competition_state_cb, this, std::placeholders::_1));
-// }
-
-void AriacCompetition::competition_state_cb( const ariac_msgs::msg::CompetitionState::ConstSharedPtr msg) 
-{
-  if (msg->competition_state == ariac_msgs::msg::CompetitionState::READY) {
-    std::string srv_name = "/ariac/start_competition";
-    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("trigger_client");
+    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("end_trigger_client");
     rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr client = node->create_client<std_srvs::srv::Trigger>(srv_name);;
 
-    
+    auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
 
-    // client = this->create_client<std_srvs::srv::Trigger>(srv_name);
+    while (!client->wait_for_service(1s)) {
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+      }
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+    }
+
+    auto result = client->async_send_request(request);
+    // Wait for the result.
+    if (rclcpp::spin_until_future_complete(node, result) == rclcpp::FutureReturnCode::SUCCESS) {
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
+                  "client returned result %d",
+                  result.get()->success);
+    }
+    else
+    {
+      RCLCPP_ERROR_STREAM(
+        rclcpp::get_logger("rclcpp"), "Failed to call trigger service");
+    }
+  }
+}
+
+
+
+void AriacCompetition::competition_state_cb( const ariac_msgs::msg::CompetitionState::ConstSharedPtr msg)  {
+  competition_state_ = msg->competition_state;
+  if (msg->competition_state == ariac_msgs::msg::CompetitionState::READY) {
+    std::string srv_name = "/ariac/start_competition";
+
+    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("start_trigger_client");
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr client = node->create_client<std_srvs::srv::Trigger>(srv_name);;
 
     auto request = std::make_shared<std_srvs::srv::Trigger::Request>();
 
