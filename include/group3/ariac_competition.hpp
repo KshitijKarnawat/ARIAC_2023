5ac0c125ac27fb2ca0d573f5c5d32594989b23ce
Sanchit kedia <sanchit@terpmail.umd.edu>
2023-02-25 09:02:34 -0500

    Combine ariac_competition and rwa1

--- include/group3/ariac_competition.hpp
+++ include/group3/ariac_competition.hpp
@@ -1,14 +1,39 @@
 #pragma once
+
 #include <rclcpp/qos.hpp>
 #include <rclcpp/rclcpp.hpp>
+
+#include <cstdint>
+#include <iostream>
+#include <typeinfo>
+#include <chrono>
 #include <unistd.h>
+#include <string>
+#include <array>
+#include <vector>
+#include <queue>
+#include <memory>
+
 #include <ament_index_cpp/get_package_share_directory.hpp>
+
 #include <std_srvs/srv/trigger.hpp>
+
 #include <ariac_msgs/msg/order.hpp>
 #include <ariac_msgs/msg/competition_state.hpp>
+#include <ariac_msgs/msg/part.hpp>
+#include <ariac_msgs/msg/assembly_task.hpp>
+#include <ariac_msgs/msg/kitting_task.hpp>
+#include <ariac_msgs/msg/combined_task.hpp>
+#include <ariac_msgs/msg/kitting_part.hpp>
+#include <ariac_msgs/msg/assembly_part.hpp>
+
 #include <ariac_msgs/srv/submit_order.hpp>
 
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/vector3.hpp>
+
 using namespace std::chrono_literals;
+
 class AriacCompetition : public rclcpp::Node
 {
  public:
@@ -23,15 +48,78 @@ class AriacCompetition : public rclcpp::Node
         // rclcpp::Parameter pub_frequency = this->get_parameter("publishing_interval");
 
         end_competition_timer_ = this->create_wall_timer(100ms, std::bind(&AriacCompetition::end_competition_timer_callback, this));
+
+        order_subscriber = this->create_subscription<ariac_msgs::msg::Order>("/ariac/orders", 10, std::bind(&AriacCompetition::order_callback, this, std::placeholders::_1));
     }
+
  
  private:
     rclcpp::Subscription<ariac_msgs::msg::CompetitionState>::SharedPtr competition_state_sub_;
 
     rclcpp::TimerBase::SharedPtr end_competition_timer_;
     
+    ariac_msgs::msg::Order order_;
+
+    rclcpp::Subscription<ariac_msgs::msg::Order>::SharedPtr order_subscriber;
+
     void competition_state_cb(const ariac_msgs::msg::CompetitionState::ConstSharedPtr msg);
 
     void end_competition_timer_callback();
 
+    void order_callback(const ariac_msgs::msg::Order::SharedPtr msg);
+
+    void submit_order(std::string order_id);
+
 };
+
+class Orders {
+
+ public:
+    std::string id;
+    bool priority;
+    int type;
+    
+    struct KittingTask{
+        int agv_id;
+        int tray_id;
+        int destination;
+        std::array<int, 3> part;    // 0-color, 1-type, 2-quadrant
+        std::vector<std::array<int, 3>> parts_kit;
+    };
+
+    KittingTask KittingTask_var;
+
+    struct AssemblyTask{
+        std::vector<int> agv_numbers;
+        int station;
+        struct part{
+            int type;
+            int color;
+            geometry_msgs::msg::PoseStamped assembled_pose;
+            geometry_msgs::msg::Vector3 install_direction;
+        };
+        part part_var;
+        std::vector<part> parts_assm;
+    };
+
+    AssemblyTask AssemblyTask_var;
+
+    struct CombinedTask{
+        int station;
+        struct part{
+            int type;
+            int color;
+            geometry_msgs::msg::PoseStamped assembled_pose;
+            geometry_msgs::msg::Vector3 install_direction;
+        };
+        part part_var;
+        std::vector<part> parts_comb;
+    };
+
+    CombinedTask CombinedTask_var;
+
+    Orders();
+
+};
+
+std::vector<Orders> orders;
--- src/ariac_competition.cpp
+++ src/ariac_competition.cpp
@@ -1,4 +1,6 @@
-#include "ariac_competition.hpp"
+#include "../include/group3/ariac_competition.hpp"
+
+Orders::Orders(void){}
 
 void AriacCompetition::end_competition_timer_callback() {
   if (competition_state_ == 3 && submit_orders_ == 1) {
@@ -62,8 +64,103 @@ void AriacCompetition::competition_state_cb( const ariac_msgs::msg::CompetitionS
       RCLCPP_ERROR_STREAM(
         rclcpp::get_logger("rclcpp"), "Failed to call trigger service");
     }
+  }
 }
- 
+
+void AriacCompetition::order_callback(ariac_msgs::msg::Order::SharedPtr msg){
+
+    // RCLCPP_INFO(this->get_logger(), "Order ID CB: ");
+    Orders order;
+    order.id = msg->id;
+    order.priority = msg->priority;
+    order.type = msg->type;
+
+    if (order.type == 0){
+        // RCLCPP_INFO(this->get_logger(), "KITTING: ");
+        order.KittingTask_var.agv_id = msg->kitting_task.agv_number;
+        order.KittingTask_var.tray_id = msg->kitting_task.tray_id;
+        order.KittingTask_var.destination = msg->kitting_task.destination;
+        for (unsigned int i = 0 ; i < msg->kitting_task.parts.size(); i++)
+        {
+            order.KittingTask_var.part[0] = msg->kitting_task.parts[i].part.color;
+            order.KittingTask_var.part[1] = msg->kitting_task.parts[i].part.type;
+            order.KittingTask_var.part[2] = msg->kitting_task.parts[i].quadrant;
+            order.KittingTask_var.parts_kit.push_back(order.KittingTask_var.part);
+        }
+    }
+
+    else if (order.type == 1){
+        // RCLCPP_INFO(this->get_logger(), "ASSEM: ");
+        // RCLCPP_INFO_STREAM(this->get_logger(), msg->assembly_task.agv_numbers);
+        // RCLCPP_INFO_STREAM(this->get_logger(), "0: " << msg->assembly_task.agv_numbers[0].data);
+        // RCLCPP_INFO_STREAM(this->get_logger(), "1: " << int(msg->assembly_task.agv_numbers.at(0)));
+        for (unsigned i = 0; i < msg->assembly_task.agv_numbers.size(); i++){
+            order.AssemblyTask_var.agv_numbers.push_back(int(msg->assembly_task.agv_numbers.at(i)));
+            // RCLCPP_INFO_STREAM(this->get_logger(), int(msg->assembly_task.agv_numbers.at(i)));
+        }
+        
+        order.AssemblyTask_var.station = msg->assembly_task.station;
+        for (unsigned int i = 0 ; i < msg->assembly_task.parts.size(); i++)
+        {
+            order.AssemblyTask_var.part_var.type = msg->assembly_task.parts[i].part.type;
+            order.AssemblyTask_var.part_var.color = msg->assembly_task.parts[i].part.color;
+            order.AssemblyTask_var.part_var.assembled_pose = msg->assembly_task.parts[i].assembled_pose;
+            order.AssemblyTask_var.part_var.install_direction = msg->assembly_task.parts[i].install_direction;
+            // RCLCPP_INFO(this->get_logger(), "pushback");
+            order.AssemblyTask_var.parts_assm.push_back(order.AssemblyTask_var.part_var);
+        }
+    }
+
+    else if (order.type == 2){
+        // RCLCPP_INFO(this->get_logger(), "COMB");
+        order.CombinedTask_var.station = msg->combined_task.station;
+        for (unsigned int i = 0 ; i < msg->combined_task.parts.size(); i++)
+        {
+            order.CombinedTask_var.part_var.type = msg->combined_task.parts[i].part.type;
+            order.CombinedTask_var.part_var.color = msg->combined_task.parts[i].part.color;
+            order.CombinedTask_var.part_var.assembled_pose = msg->combined_task.parts[i].assembled_pose;
+            order.CombinedTask_var.part_var.install_direction = msg->combined_task.parts[i].install_direction;
+            order.CombinedTask_var.parts_comb.push_back(order.CombinedTask_var.part_var);
+        }
+    }
+    // RCLCPP_INFO(this->get_logger(), "Before If");
+    if (order.priority == 0 || orders.size() == 0) {
+        orders.push_back(order);
+        // RCLCPP_INFO(this->get_logger(), "Order ID 1: %s", order.id.c_str());
+    }
+    else if (orders[orders.size()-1].priority == 1){
+        orders.push_back(order);
+        // RCLCPP_INFO(this->get_logger(), "Order ID 2: %s", order.id.c_str());
+    }
+    else { 
+        // RCLCPP_INFO(this->get_logger(), "Order ID 3: %s", order.id.c_str());
+        for (unsigned int i = 0; i<orders.size(); i++) {
+            if (orders[i].priority == 0) {
+                orders.insert(i+orders.begin(), order);
+                break;
+            }
+            
+        }
+    }
+    
+    if (orders.size() == 3) {
+        for (auto i = 0; i < 3; i++) {
+            RCLCPP_INFO(this->get_logger(), "Submitting order: %s", orders[i].id.c_str());
+            submit_order(orders[i].id.c_str());
+        }
+    }
+    // RCLCPP_INFO(this->get_logger(), "Order ID: %s", order.id.c_str());
+}
+
+void AriacCompetition::submit_order(std::string order_id)
+{
+  rclcpp::Client<ariac_msgs::srv::SubmitOrder>::SharedPtr client;
+  client = this->create_client<ariac_msgs::srv::SubmitOrder>("/ariac/submit_order");
+  auto request = std::make_shared<ariac_msgs::srv::SubmitOrder::Request>();
+  request->order_id = order_id;
+
+  auto result = client->async_send_request(request);
+  RCLCPP_INFO(this->get_logger(), "Submitted order: %s", order_id.c_str());
 }
 
 int main(int argc, char *argv[])
