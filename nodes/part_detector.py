ea3311dc57b6a4f8e34ab4af50bbb7f6c64ca125
Tanmay <tanmay.n.haldankar@gmail.com>
2023-04-12 16:03:08 -0400

    Add conveyor part detection

--- nodes/part_detector.py
+++ nodes/part_detector.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-
-# # Credits:
-# - Addison Sears-Collins
-# - https://automaticaddison.com
    
 # Import the necessary libraries
 import rclpy # Python library for ROS 2
@@ -43,8 +39,17 @@ class ImageSubscriber(Node):
       10)
     self.subscription2 # prevent unused variable warning
 
+    self.subscription3 = self.create_subscription(
+      Image, 
+      # '/ariac/sensors/kts1_rgb_camera/rgb_image',
+      '/ariac/sensors/conv_rgb_camera/rgb_image',
+      self.listener_callback3, 
+      10)
+    self.subscription3 # prevent unused variable warning
+
     self.publisher1_ = self.create_publisher(Partsmsg, 'right_bin_part_detector', 10)
     self.publisher2_ = self.create_publisher(Partsmsg, 'left_bin_part_detector', 10)
+    self.publisher3_ = self.create_publisher(Partmsg, 'conveyor_part_detector', 10)
        
     # Used to convert between ROS and OpenCV images
     self.br = CvBridge()
@@ -261,6 +266,100 @@ class ImageSubscriber(Node):
     self.publisher2_.publish(msg2)
     # self.destroy_node()
 
+  def listener_callback3(self, data):
+    # msg3 = Partmsg()
+    """
+    Callback function.
+    """
+    # Display the message on the console
+    # self.get_logger().info('Receiving video frame')
+  
+    # Convert ROS Image message to OpenCV image
+    current_frame = self.br.imgmsg_to_cv2(data)
+    current_frame = cv2.cvtColor(current_frame, cv2.COLOR_RGB2BGR)
+
+    # Display image
+    img = current_frame
+    img = img[:, 223:443]
+    img_hsv = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2HSV)
+    lower_red = np.array([0, 55, 25])
+    upper_red = np.array([10, 255, 255])
+    lower_red1 = np.array([170, 55, 25])
+    upper_red1 = np.array([180, 255, 255])
+
+    lower_green = np.array([36, 55, 25])
+    upper_green = np.array([70, 255, 255])
+
+    lower_blue = np.array([110, 55, 25])
+    upper_blue = np.array([130, 255, 255])
+
+    lower_orange = np.array([6, 55, 25])
+    upper_orange = np.array([26, 255, 255])
+
+    lower_purple = np.array([128, 55, 25])
+    upper_purple = np.array([148, 255, 255])
+
+    lower_gray = np.array([0, 0, 32])
+    upper_gray = np.array([0, 255, 117])
+    
+    mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
+    mask11 = cv2.inRange(img_hsv, lower_red1, upper_red1)
+
+    mask2 = cv2.inRange(img_hsv, lower_green, upper_green)
+
+    mask3 = cv2.inRange(img_hsv, lower_blue, upper_blue)
+
+    mask4 = cv2.inRange(img_hsv, lower_orange, upper_orange)
+
+    mask5 = cv2.inRange(img_hsv, lower_purple, upper_purple)
+
+    mask6 = cv2.inRange(img_hsv, lower_gray, upper_gray)
+
+    mask = mask1 + mask2 + mask3 + mask4 + mask5 + mask6 + mask11
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel=np.ones((3, 3), np.uint8), iterations=2)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel=np.ones((3, 3), np.uint8), iterations=1)
+
+    new_image = cv2.bitwise_and(img, img, mask = mask)
+    blur = cv2.GaussianBlur(mask,(5,5), 0)
+    contours, hierarchy = cv2.findContours(blur, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    for c in contours:
+      msg = Partmsg()
+      M = cv2.moments(c)
+      if M['m00'] != 0.0:
+        x_m = int(M['m10']/M['m00'])
+        y_m = int(M['m01']/M['m00'])
+
+      msg.quad = 0
+      img_hsv = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2HSV)
+      hsv = img_hsv[int(y_m),int(x_m)]
+
+      if (hsv[0]>10 and hsv[0]<25):
+        msg.color = Partmsg.ORANGE
+        # self.get_logger().info("Orange")
+        msg.type = self.conveyor_detect_type(new_image.copy(), c)
+      elif(hsv[0]>=130 and hsv[0]<170):
+        msg.color = Partmsg.PURPLE
+        # self.get_logger().info("Purple")
+        msg.type = self.conveyor_detect_type(new_image.copy(), c)
+      elif(hsv[0]>=90 and hsv[0]<130):
+        msg.color = Partmsg.BLUE
+        # self.get_logger().info("Blue")
+        msg.type = self.conveyor_detect_type(new_image.copy(), c)
+      elif(hsv[0]>=0 and hsv[0]<=10):
+        msg.color = Partmsg.RED
+        # self.get_logger().info("Red")
+        msg.type = self.conveyor_detect_type(new_image.copy(), c)
+      elif(hsv[0]>36 and hsv[0]<89):
+        msg.color = Partmsg.GREEN
+        # self.get_logger().info("Green")
+        msg.type = self.conveyor_detect_type(new_image.copy(), c)
+      # parts_.append(msg)
+
+# self.get_logger().info("\n")
+      self.publisher3_.publish(msg)
+# self.destroy_node()
+
   def detect_type(self,img, cnt):
     x, y, w, h = cv2.boundingRect(cnt)
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
@@ -296,7 +395,7 @@ class ImageSubscriber(Node):
       elif perimeter > 151 and 23<area_gray<203:
         # self.get_logger().info("Sensor")
         return Partmsg.SENSOR
-    elif (count == 0):
+    else:
       # self.get_logger().info("Pump")
       return Partmsg.PUMP
 
@@ -378,6 +477,43 @@ class ImageSubscriber(Node):
 
     return q
  
+  def conveyor_detect_type(self,img, cnt):
+    x, y, w, h = cv2.boundingRect(cnt)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    im_hsv = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2HSV)
+
+    lower_gray = np.array([0, 0, 32])
+    upper_gray = np.array([0, 0, 117])
+    mask = cv2.inRange(im_hsv, lower_gray, upper_gray)
+
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel=np.ones((3, 3), np.uint8), iterations=2)
+    
+    contours, __ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    count = 0
+
+    for c in contours:
+      area_gray = cv2.contourArea(c)
+      perimeter = cv2.arcLength(cnt,True)
+      if area_gray > 10:
+          count += 1 
+    if (count == 2):
+      # self.get_logger().info("Regulator")
+      return Partmsg.REGULATOR
+    elif (count == 1):
+      if perimeter < 250:
+        # self.get_logger().info("Battery")
+        return Partmsg.BATTERY
+      elif area_gray < 20:
+        # self.get_logger().info("Pump")
+        return Partmsg.PUMP
+      elif perimeter > 250:
+        # self.get_logger().info("Sensor")
+        return Partmsg.SENSOR
+    else:
+      # self.get_logger().info("Pump")
+      return Partmsg.PUMP
+
 def main(args=None):
    
   # Initialize the rclpy library
